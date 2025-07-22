139feabcdad30a6ddf1210edb9f1f973f93ae652
Julian Enrique Luque Lopez <julianenriqueluquelopez@gmail.com>
2025-07-22 10:23:07 -0600

    Conexion a mongodb establecida y aplicada para realizar rutas y request de backend

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Servidor corriendo")
+import express from 'express';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI || '';
+console.log(MONGO_URI)
+
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("💚 Conectado a la base de datos")
+}).catch((err)=>{
+    console.error("💔 Error al conectar a la base de datos: ", err)
+})
+
+app.get("/",(_req, res)=>{
+    res.send("Servidor funcionando 🚀");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor alojado en: http://localhost:${PORT}`)
+})
+
